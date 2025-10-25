2e7c9759a2dbb4fae9fbebf4493c6288c0580300
javohir <HayitboyevJavohir>
2025-10-25 12:03:17 +0500

    telegram bot yaratdim

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import TelegramBot from "node-telegram-bot-api";
 
 const TOKEN = "8321889241:AAFjb5oc_Fk0obDSxoWC9ykwkDQLnCzObP0";
-const bot = new 
+const bot = new TelegramBot(TOKEN, {polling: true});
 const bot = new TelegramBot();
+
+bot.on("message", function (msg) {
+    const chatID = msg.chat.id;
+    const text = msg.text;
+    console.log(msg);
+
+    bot.sendMessage(chatID, `--> ${text}`);
+})
+
+console.log("Bot ishga tushdi...");
