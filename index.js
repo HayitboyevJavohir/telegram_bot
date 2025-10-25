2eec0c72eb5e00434fe4f5de8443668ab22e36a5
javohir <HayitboyevJavohir>
2025-10-25 12:17:58 +0500

    telegram bot ishga tushdi

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import TelegramBot from "node-telegram-bot-api";
 
 const TOKEN = "8321889241:AAFjb5oc_Fk0obDSxoWC9ykwkDQLnCzObP0";
 const bot = new TelegramBot(TOKEN, {polling: true});
-const bot = new TelegramBot();
 
 bot.on("message", function (msg) {
     const chatID = msg.chat.id;
@@ -12,4 +11,4 @@ bot.on("message", function (msg) {
     bot.sendMessage(chatID, `--> ${text}`);
 })
 
-console.log("Bot ishga tushdi...");
+console.log("bot ishga tushdi...")
