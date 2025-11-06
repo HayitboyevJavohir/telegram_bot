9ae1b097e3f9ad2c948973d45e16d89512445f70
javohir <HayitboyevJavohir>
2025-11-06 12:03:57 +0500

    done

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 import TelegramBot from "node-telegram-bot-api";
 
 const TOKEN = "8321889241:AAFjb5oc_Fk0obDSxoWC9ykwkDQLnCzObP0";
-const bot = new TelegramBot(TOKEN, {polling: true});
+const bot = new TelegramBot(TOKEN, { polling: true });
 
-bot.on("message", function (msg) {
-    const chatID = msg.chat.id;
-    const text = msg.text;
-    const firstName = msg.chat.first_name;
-    console.log(msg);
+bot.on("message", async function (msg) {
+  const chatID = msg.chat.id;
+  const text = msg.text;
+  const firstName = msg.chat.first_name;
+  console.log(msg);
 
-    if (text == "/start") {
-        bot.sendMessage(chatID, ` Xush kelibsiz, ${firstName}`, {
-            reply_markup: {
-                keyboard: [
-                    [{ text: "Boshlash" }],
-                    [{ text: "Menu" }, {text: "Til"}]
-                ],
-                resize_keyboard: true,
-            },
-        });
-    } else if (text == "/help") {
-        bot.sendMessage(chatID, ` Sizga qanday yordam kerak, ${firstName}? `);
-    } else if (text == "/sozlamalar") {
-        bot.sendMessage(chatID, `Bu sozlamalar, ${firstName}`);
-    } else if (text == "Boshlash") {
-        bot.sendMessage(chatID, `Boshlanyapti...`);
-    } else if (text == "Menu") {
-        bot.sendMessage(chatID, `Menyuuuuuu...`);
-    } else if (text == "Til") {
-        bot.sendMessage(chatID, ` Tilni tanlang... `);
-    }
+  if (text == "/start") {
+    bot.sendMessage(chatID, ` Xush kelibsiz, ${firstName}`, {
+      reply_markup: {
+        keyboard: [[{ text: "Boshlash" }], [{ text: "Menu" }, { text: "Til" }]],
+        resize_keyboard: true,
+      },
+    });
+  } else if (text == "/help") {
+    bot.sendMessage(chatID, ` Sizga qanday yordam kerak, ${firstName}? `);
+  } else if (text == "/sozlamalar") {
+    bot.sendMessage(chatID, `Bu sozlamalar, ${firstName}`);
+  } else if (text == "Boshlash") {
+    const xabar = await bot.sendMessage(chatID, `iltimos kuting...`)
+    setTimeout(function () {
+
+      bot.deleteMessage(chatID, xabar.message_id);
+      bot.sendPhoto(chatID, "./images/bugatti.jpg", {
+        caption: `Automobiles Ettore Bugatti was a French manufacturer of high-performance automobiles. The company was founded in 1909 in the then-German city of Molsheim`,
+      });
+
+      console.log("2 sekundga kechiktirilsin");
+    }, 1 * 1000);
+  } else if (text == "Menu") {
+    bot.sendMessage(chatID, `Menyuuuuuu...`);
+  } else if (text == "Til") {
+    bot.sendMessage(chatID, ` Tilni tanlang... `);
+  }
 });
 
-console.log("bot ishga tushdi...")
+console.log("bot ishga tushdi...");
